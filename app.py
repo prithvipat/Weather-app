8773477e33a6ed63ea26ae06bcbfd1d4ae58191c
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-03-16 19:03:25 -0400

    Update app.py
    Changes to different html file if city name doesn't exist

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
- 
 from flask import Flask, request, render_template
 from weather import weather
+from datetime import datetime
+
+day = datetime.today().weekday()
+weekdates = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',]
+weekday = weekdates[day]
+arr = [weekdates[day + 1], weekdates[day + 2], weekdates[day + 3], weekdates[day + 4]]
 
 app = Flask(__name__)
 
@@ -13,13 +18,21 @@ def index():
 def get_weather():
     city = request.args.get('city')
     weather_data = weather(city)
-    return render_template(
-        "weather.html",
-        title=weather_data['name'],
-        temp=f"{weather_data['main']['temp']:.1f}",
-        description=f"{weather_data['weather'][0]['description']}",
-        wind=f"{weather_data['main']['feels_like']}"
-    )
+
+    if weather_data == 'Error':
+        return render_template(
+            "error.html",
+        )
+    
+    else:
+        return render_template(
+            "weather.html",
+            day=weekday,
+            title=weather_data['name'],
+            temp=f"{weather_data['main']['temp']:.1f}",
+            description=f"{weather_data['weather'][0]['description']}",
+            wind=f"{weather_data['main']['feels_like']}"
+            )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0",port=8000)
