5e6d4c54d1ceeec63064ac300556f36f3347cbae
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-03-24 12:44:02 -0400

    Update weather.py
    Shows the weather for the next 2 days also images based on the weather

--- weather.py
+++ weather.py
@@ -2,8 +2,8 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-
 base_url = "https://api.openweathermap.org/data/2.5/weather?{city}"
+
 def weather(city_name):
     params = {
         "q": city_name,
@@ -11,7 +11,7 @@ def weather(city_name):
         "units": "metric",
     }
 
-    try:
+    try: 
         response = requests.get(base_url, params=params)
         response.raise_for_status()
         data = response.json()
@@ -21,3 +21,67 @@ def weather(city_name):
         return('Error')
     
     return data
+
+# Getting weather data for the upcoming days
+def forecast(city_name): 
+    base_url = "https://api.openweathermap.org/data/2.5/forecast?"
+    params = {
+    "q": city_name,
+    "appid": "API KEY",
+    "units": "metric",
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    return data
+
+# Images based on weather conditions
+def images(city_name):
+    data = weather(city_name)
+    description = f"{data['weather'][0]['description']}"
+    word_list = description.split()
+
+    if word_list[-1] == 'snow':
+        return 'snow.png'
+    
+    elif word_list[-1] == 'rain':
+        return 'raining.png'
+    
+    elif word_list[-1] == 'haze':
+        return 'sun_moreclouds.png'
+    
+    elif description == 'scattered clouds':
+        return 'sun_clouds.png'
+    
+    elif word_list[-1] == 'clouds':
+        return 'clouds.png'
+    
+    else: 
+        return 'sun.png'
+    
+def forecast_img(city_name, day):
+    data = forecast(city_name)
+    description = data['list'][day]['weather'][0]['description']
+    word_list = description.split()
+
+    if word_list[-1] == 'snow':
+        return 'snow.png'
+    
+    elif word_list[-1] == 'rain':
+        return 'raining.png'
+    
+    elif word_list[-1] == 'haze':
+        return 'sun_moreclouds.png'
+    
+    elif description == 'scattered clouds':
+        return 'sun_clouds.png'
+    
+    elif word_list[-1] == 'clouds':
+        return 'clouds.png'
+    
+    else: 
+        return 'sun.png'
+
+
+    
